462846c39e37cba87ac662c6cda24160f7db76f6
Teemu Frisk <teemu.frisk@eficode.fi>
2011-12-12 19:32:04 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodbase::Application.config.secret_token = '6fe9a1e680574f1b8d9c42f0962aa749c6d4d1b949c4724a5dfcced5026c3325bfb1d4f9562df33a64e0bd06f920bc69e3969ee95a07e6b50d73ca3b6e4a3fe4'
