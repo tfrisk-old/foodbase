caef21a52fe97aa9ed333cf9a32438939f14f205
Teemu Frisk <teemu.frisk@eficode.fi>
2011-12-12 21:15:19 +0200

    Add fooditem edit page

--- app/controllers/fooditems_controller.rb
+++ app/controllers/fooditems_controller.rb
@@ -7,4 +7,18 @@ class FooditemsController < ApplicationController
     @fooditem = Fooditem.find(params[:id])
   end
 
+  def edit
+    @fooditem = Fooditem.find(params[:id])
+  end
+
+  def update
+    @fooditem = Fooditem.find(params[:id])
+    if @fooditem.update_attributes(params[:fooditem])
+      flash[:notice] = "Homma OK"
+      redirect_to fooditem_path(@fooditem)
+    else
+      render action: :edit
+    end
+  end
+
 end
