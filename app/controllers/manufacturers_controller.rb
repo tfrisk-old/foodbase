62e5db84fd39b6f808db85159bbbbf8dea50b159
Teemu Frisk <teemu.frisk@eficode.fi>
2012-01-16 21:56:28 +0200

    Add remaining views for manufacturer

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -13,4 +13,33 @@ class ManufacturersController < ApplicationController
     @manufacturer = Manufacturer.find(params[:id])
   end
 
+  def edit
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def update
+    @manufacturer = Manufacturer.find(params[:id])
+    if @manufacturer.update_attributes(params[:manufacturer])
+      flash[:notice] = "Homma OK"
+      redirect_to manufacturer_path(@manufacturer)
+    else
+      render action: :edit
+    end
+  end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+
+  def create
+    @manufacturer = Manufacturer.new(params[:manufacturer])
+    if @manufacturer.save
+      flash[:notice] = "Tallennettu"
+      redirect_to manufacturers_path
+    else
+      render action: :new
+    end
+  end
+
 end
